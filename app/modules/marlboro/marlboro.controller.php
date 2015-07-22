36687ea2f11630abb1b0332926f10c53b1e74ebb
sngrl <reserved@mail.ru>
2015-07-22 15:39:53 +0300

    Marlboro Fixes

--- app/modules/marlboro/marlboro.controller.php
+++ app/modules/marlboro/marlboro.controller.php
@@ -106,7 +106,7 @@ class MarlboroController extends BaseController {
         $token = $this->token;
 
         $cities = YaDiskVideo::select('city')->distinct()->orderBy('city', 'asc')->lists('city', 'city');
-        Helper::tad($cities);
+        #Helper::tad($cities);
 
         return View::make($this->module['gtpl'].'yadisk_token', compact('faces', 'token'));
     }
